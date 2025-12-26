185751f4aa156181f7e856559964d00f72ff5a0f
LuisM <muislarques@gmail.com>
2025-12-26 19:17:01 +0000

    changes to compiller and debugger.

---
+++ skidpad/eighen_test.cpp
@@ -0,0 +1,10 @@
+// #include <Eigen/Dense>
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+    // Eigen::Vector2d v(1, 2);
+    // std::cout << v.norm() << std::endl;
+    // return 0;
+}
