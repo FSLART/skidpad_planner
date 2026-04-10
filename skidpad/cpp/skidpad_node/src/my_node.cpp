2cb2a2c0b93056ea64b0289f2de8f8d8bf366ffb
Gabriel Januário Tomás <gabrieljtomas@gmail.com>
2026-04-10 17:32:56 +0100

    Topics updated

--- skidpad/cpp/skidpad_node/src/my_node.cpp
+++ skidpad/cpp/skidpad_node/src/my_node.cpp
@@ -12,8 +12,8 @@ skidpad_node::skidpad_node() : Node("skidpadNode")
 {
     RCLCPP_INFO(this->get_logger(), "Skidpad node has been started");
 
-    this->path_vis_pub = this->create_publisher<nav_msgs::msg::Path>("/slam/pose", 10);
-    this->path_control_pub = this->create_publisher<lart_msgs::msg::PathSpline>("/planning/path", 10);
+    this->path_vis_pub = this->create_publisher<nav_msgs::msg::Path>("/planned_path_marker", 10);
+    this->path_control_pub = this->create_publisher<lart_msgs::msg::PathSpline>("/planned_path_topic", 10);
 
     this->cone_array_subscriber = this->create_subscription<lart_msgs::msg::ConeArray>("/mapping/cones", 10, std::bind(&skidpad_node::coneArrayCallback, this, _1));
     this->position_subscriber = this->create_subscription<geometry_msgs::msg::PoseStamped>("/slam/pose", 10, std::bind(&skidpad_node::positionCallback, this, _1));
